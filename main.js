dd3d7c4b09ff9a00354d32c6647619ce96f904da
ctvanzandt42 <ctvanzandt42@gmail.com>
2017-09-09 19:01:53 -0500

    Fixed it, was testing something w/ surge

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function basicInfo() {
 
     let data = JSON.parse(this.responseText);
     let basic_info =
-        `<p>Nae: ${data.name}<br>
+        `<p>Name: ${data.name}<br>
         GitHub URL: <a href="${data.url}">ctvanzandt42<a><br>
         Email: <a href=ctvanzandt42@gmail.com>ctvanzandt42@gmail.com<a><br>
         Company: ${data.company}<br>
